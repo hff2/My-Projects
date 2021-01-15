7bcf7a2bfcf298b038e54f2503845f988eb49db2
hff2 <hophoebeho@gmail.com>
2021-01-15 12:02:56 +0800

    add EventListener for submit on the form

---
+++ Form_Validator/app.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("submit");
+});
