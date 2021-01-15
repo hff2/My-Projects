b3bfc765345d419cbccfc5b5e73e0a1452fd0eaa
hff2 <hophoebeho@gmail.com>
2021-01-15 12:09:16 +0800

    add showError and showSuccess function to check the input element

--- Form_Validator/app.js
+++ Form_Validator/app.js
@@ -4,7 +4,26 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+//Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+//Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("submit");
+  if (username.value === "") {
+    showError(username, "Usrename is required");
+  } else {
+    showSuccess(username);
+  }
 });
