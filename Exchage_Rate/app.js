d808d18510127a150ab209d96249672479944ed8
hff2 <hophoebeho@gmail.com>
2021-01-18 15:22:41 +0800

    add variable to read elements changes

---
+++ Exchage_Rate/app.js
@@ -0,0 +1,20 @@
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+//Fetch exchage rates and update the DOM
+function caclulate() {
+  console.log("RUN");
+}
+
+// Event listeners
+currencyEl_one.addEventListener("change", caclulate);
+amountEl_one.addEventListener("input", caclulate);
+currencyEl_two.addEventListener("change", caclulate);
+amountEl_two.addEventListener("input", caclulate);
+
+caclulate();
