61e64becadc925661768c1017224354f5b17ff01
hff2 <hophoebeho@gmail.com>
2021-01-20 13:52:25 +0800

    add updateValues function to display balance,income and expense

--- Expense_Tracker/app.js
+++ Expense_Tracker/app.js
@@ -33,10 +33,29 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+//Update the balance,income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
 //Init app
 function init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
