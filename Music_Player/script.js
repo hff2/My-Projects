4d0db95ec97bafccaecc203c6b8d52aeb167161e
hff2 <hophoebeho@gmail.com>
2021-01-21 22:11:37 +0800

    when the music finished it will switch next song

--- Music_Player/script.js
+++ Music_Player/script.js
@@ -107,3 +107,6 @@ audio.addEventListener("timeupdate", updateProgress);
 
 // Click on progress bar
 progressContainer.addEventListener("click", setProgress);
+
+// Song ends
+audio.addEventListener("ended", nextSong);
