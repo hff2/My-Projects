eb0880b2481aeab37d111e30a67290d15128ba37
hff2 <hophoebeho@gmail.com>
2021-01-22 14:53:45 +0800

    complete switch song

--- Music_Player/jquery.js
+++ Music_Player/jquery.js
@@ -20,6 +20,26 @@ function pauseSong() {
   $("#audio")[0].pause();
 }
 
+// Next song
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
 $("#play").click(function () {
   const isPlaying = $("#music-container").hasClass("play");
   if (isPlaying == true) {
@@ -27,4 +47,12 @@ $("#play").click(function () {
   } else {
     playSong();
   }
+});
+
+// Change song
+$("#prev").click(function () {
+  prevSong();
+});
+$("#next").click(function () {
+  nextSong();
 });
