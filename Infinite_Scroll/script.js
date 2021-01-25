058a7aad5587fb27b918fca76f7c4ad990ac2532
hff2 <hophoebeho@gmail.com>
2021-01-25 14:14:37 +0800

    use filter to fetch post

--- Infinite_Scroll/script.js
+++ Infinite_Scroll/script.js
@@ -49,6 +49,23 @@ function showLoading() {
   }, 1000);
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
+
 //Show initial posts
 showPosts();
 
