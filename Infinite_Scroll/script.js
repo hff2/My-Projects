9a813bb42f76354da8b3a5196ce4c18f5d1b88dc
hff2 <hophoebeho@gmail.com>
2021-01-25 13:31:58 +0800

    get initial posts from api

---
+++ Infinite_Scroll/script.js
@@ -0,0 +1,39 @@
+const postsContainer = document.getElementById("posts-container");
+const loading = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 3;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+//Show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+//Show initial posts
+showPosts();
