139e05872aa7a1ea7e780194bc1d54690cdd6770
hff2 <hophoebeho@gmail.com>
2021-01-25 21:43:48 +0800

    use jquery to get initial posts from api

---
+++ Infinite_Scroll/jquery.js
@@ -0,0 +1,25 @@
+let limit = 5;
+let page = 1;
+
+function showPosts() {
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`,
+    method: "get",
+    dataType: "json",
+    success: function (data) {
+      $.each(data, function (index, post) {
+        const postEl = $("<div>").addClass("post");
+        postEl.html(`
+        <div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">${post.body}</p>
+        </div>
+      `);
+        $("#posts-container").append(postEl);
+      });
+    },
+  });
+}
+
+showPosts();
