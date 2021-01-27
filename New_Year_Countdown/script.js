d0f17d2c5732580ed5f34b46ef79270a0f40e0f3
hff2 <hophoebeho@gmail.com>
2021-01-27 13:08:18 +0800

    dynamic year and spinner gif

--- New_Year_Countdown/script.js
+++ New_Year_Countdown/script.js
@@ -3,11 +3,17 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
@@ -23,4 +29,11 @@ function updateCountdown() {
   seconds.innerHTML = s < 10 ? "0" + s : s;
 }
 
+//Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
+//Run every second
 setInterval(updateCountdown, 1000);
