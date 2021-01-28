40a2fd59aea7a65a25708202a52b8726f88dfc66
hff2 <hophoebeho@gmail.com>
2021-01-28 14:35:45 +0800

    use jquery to implement timer and game over panel

--- Typing_Game/jquery.js
+++ Typing_Game/jquery.js
@@ -1,6 +1,8 @@
 let score = 0;
 let time = 10;
 
+const timeInterval = setInterval(updateTime, 1000);
+
 function getRandomWord() {
   $.ajax({
     url: "https://random-word-api.herokuapp.com/word?number=1",
@@ -26,5 +28,35 @@ $("#text").on("input", function () {
     updateScore();
     getRandomWord();
     $(this).val("");
+
+    if (difficulty == "hard") {
+      time += 3;
+    } else if (difficulty == "medium") {
+      time += 6;
+    } else {
+      time += 8;
+    }
+    updateTime();
   }
 });
+
+function updateTime() {
+  var timeEl = $("#time");
+  time--;
+  timeEl.html(time + "s");
+  if (time === 0) {
+    clearInterval(time);
+    gameOver();
+  }
+}
+
+function gameOver() {
+  const endgameEl = $("#end-game-container");
+  endgameEl.html(`
+  <h1>Time ran out</h1>
+  <p>Your final score is ${score}</p>
+  <button onclick="location.reload()">Reload</button>
+  `);
+
+  endgameEl.css("display", "flex");
+}
