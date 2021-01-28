0867d7ac9cd66da0564d7ecaeb06c46b65c27214
hff2 <hophoebeho@gmail.com>
2021-01-28 09:59:44 +0800

    add timer and game over panel

--- Typing_Game/script.js
+++ Typing_Game/script.js
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+//Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -56,6 +62,28 @@ function addWordToDOM() {
 function updateScore() {
   score++;
   scoreEl.innerHTML = score;
+}
+
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+  if (time === 0) {
+    clearInterval(time);
+    //end the game
+    gameOver();
+  }
+}
+
+// Game over,show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <p>Your final score is ${score}</p>
+  <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = "flex";
 }
 
 addWordToDOM();
