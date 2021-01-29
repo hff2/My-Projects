9deb2c04b321c436c138311bb6ea3f5a9d616472
hff2 <hophoebeho@gmail.com>
2021-01-29 10:04:55 +0800

    add web speech api

--- Speak_Number_Guessing/script.js
+++ Speak_Number_Guessing/script.js
@@ -2,7 +2,25 @@ const msgEl = document.getElementById("msg");
 const randomNum = getRandomNumber();
 console.log("Number:", randomNum);
 
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+// Capture user speak
+function onSpeak(e) {
+  console.log(e);
+  const msg = e.results[0][0].transcript;
+  console.log(msg);
+}
+
 // Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
+
+// Speak result
+recognition.addEventListener("result", onSpeak);
