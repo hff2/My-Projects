700aacb07e8347a6fedf44765a87276dddb1b7bb
hff2 <hophoebeho@gmail.com>
2021-01-29 10:31:00 +0800

    check guessed number

--- Speak_Number_Guessing/script.js
+++ Speak_Number_Guessing/script.js
@@ -14,6 +14,7 @@ recognition.start();
 function onSpeak(e) {
   const msg = e.results[0][0].transcript;
   writeMessage(msg);
+  checkNumber(msg);
 }
 
 // Write what user speaks
@@ -24,6 +25,36 @@ function writeMessage(msg) {
   `;
 }
 
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += "<div>That is not a valid number</div>";
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += "<div>Number must be between 1 and 100</div>";
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += "<div>GO LOWER</div>";
+  } else {
+    msgEl.innerHTML += "<div>GO HIGHER</div>";
+  }
+}
+
 // Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
@@ -31,3 +62,6 @@ function getRandomNumber() {
 
 // Speak result
 recognition.addEventListener("result", onSpeak);
+
+// End SR service
+recognition.addEventListener("end", () => recognition.start());
