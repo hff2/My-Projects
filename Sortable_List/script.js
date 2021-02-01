c5405acd96b67cc6e0538838a99696a849117e59
hff2 <hophoebeho@gmail.com>
2021-02-01 16:37:18 +0800

    arrange all elements to the center

--- Sortable_List/script.js
+++ Sortable_List/script.js
@@ -27,7 +27,7 @@ function createList() {
     .map((a) => ({ value: a, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map((a) => a.value)
-    .forEach((person, index) => {
+    .forEach((company, index) => {
       const listItem = document.createElement("li");
 
       listItem.setAttribute("data-index", index);
@@ -35,7 +35,7 @@ function createList() {
       listItem.innerHTML = `
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
-          <p class="person-name">${person}</p>
+          <p class="company-name">${company}</p>
           <i class="fas fa-grip-lines"></i>
         </div>
       `;
