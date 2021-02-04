dfbb3a735b12f84319d6882fd521e046b96a9b32
hff2 <hophoebeho@gmail.com>
2021-02-04 11:31:28 +0800

    get the repository data

--- Github_Finder/script.js
+++ Github_Finder/script.js
@@ -47,5 +47,32 @@ $(document).ready(function () {
         $("#profile").html(profile);
       },
     });
+    $.ajax({
+      url: "https://api.github.com/users/" + username + "/repos",
+      data: {
+        client_id: "b9315bcd5a07fcd759d8",
+        client_secret: "a2b698bf7e7c02f898197cf136d1a41f704ca8e4",
+        sort: "created: asc",
+        per_page: 5,
+      },
+      error: function (jqXHR) {
+        var msg = "";
+        if (jqXHR.status == 404) {
+          msg = `<div class = "alert alert-danger">
+          Please enter again User does not exist</div>`;
+        }
+        $("#error").html(msg);
+
+        setTimeout(() => {
+          $("#searchUser").val("");
+          $("#repos").html("");
+          $("#error").html("");
+          $("#profile").html("");
+        }, 2300);
+      },
+      success: function insertRopo(repo_obj) {
+        console.log(repo_obj);
+      },
+    });
   });
 });
