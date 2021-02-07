0b5c71bcc47d9e6856b18214337c294ae44232b6
hff2 <hophoebeho@gmail.com>
2021-02-07 21:48:13 +0800

    implement weather information display

--- Weather_Forecast/app.js
+++ Weather_Forecast/app.js
@@ -14,7 +14,52 @@ function selectArea(data, areaIndex) {
   $(".header").html(`<h1>${areaName}</h1>`);
 }
 
+function showWeatherInfo(data, areaIndex) {
+  area = data.location;
+  let areaTemp =
+    area[areaIndex].weatherElement[1].time[0].elementValue[0].value;
+  let areaDesc =
+    area[areaIndex].weatherElement[6].time[0].elementValue[0].value;
+  let areaTempH =
+    area[areaIndex].weatherElement[5].time[0].elementValue[0].value;
+  let areaTempL =
+    area[areaIndex].weatherElement[11].time[0].elementValue[0].value;
+  $("#new_area").html(`
+  <div class="now_weather">
+    <h2>現在天氣</h2>
+    <h3>${areaDesc}</h3>
+    <p>均溫: ${areaTemp} °C</p>
+    <p class="temp">${areaTempL}°C / ${areaTempH}°C</p>
+  </div>
+    <div id="future_weather"></div>
+    `);
+
+  for (let i = 3; i < 14; i += 2) {
+    let timeIndex = i;
+    let FutureTime =
+      area[areaIndex].weatherElement[1].time[timeIndex].startTime;
+    let FutureTimeSubstr = FutureTime.substr(5, 5);
+    let FutureTemp =
+      area[areaIndex].weatherElement[1].time[timeIndex].elementValue[0].value;
+    let FutureDesc =
+      area[areaIndex].weatherElement[6].time[timeIndex].elementValue[0].value;
+    let FutureTempH =
+      area[areaIndex].weatherElement[5].time[timeIndex].elementValue[0].value;
+    let FutureTempL =
+      area[areaIndex].weatherElement[11].time[timeIndex].elementValue[0].value;
+    $("#future_weather").append(`
+    <div class="eachInfo">
+      <h1>${FutureTimeSubstr}</h1>
+      <h3>${FutureDesc}</h3>
+      <p>均溫: ${FutureTemp} °C</p>
+      <p>${FutureTempL}°C / ${FutureTempH}°C</p>
+    </div> 
+  `);
+  }
+}
+
 areaSelect.addEventListener("change", (e) => {
   areaIndex = e.target.value;
+  showWeatherInfo(data, areaIndex);
   selectArea(data, areaIndex);
 });
