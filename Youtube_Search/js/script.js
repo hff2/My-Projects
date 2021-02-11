b8b17d21c97859e822a3e5530607318257d33f3b
hff2 <hophoebeho@gmail.com>
2021-02-11 17:02:03 +0800

    add prevpage content to dom

--- Youtube_Search/js/script.js
+++ Youtube_Search/js/script.js
@@ -192,3 +192,48 @@ function nextPage() {
     }
   );
 }
+
+// Prev Page Function
+function prevPage() {
+  var token = $("#prev-button").data("token");
+  var q = $("#prev-button").data("query");
+
+  // Clear Results
+  $("#results").html("");
+  $("#buttons").html("");
+
+  // Get Form Input
+  q = $("#query").val();
+
+  // Run GET Request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search",
+    {
+      part: "snippet, id",
+      q: q,
+      pageToken: token,
+      type: "video",
+      key: "AIzaSyDUQUDfpg_6iA_ycnD5yaAYLSPc495aa8w",
+    },
+    function (data) {
+      var nextPageToken = data.nextPageToken;
+      var prevPageToken = data.prevPageToken;
+
+      // Log Data
+      console.log(data);
+
+      $.each(data.items, function (i, item) {
+        // Get Output
+        var output = getOutput(item);
+
+        // Display Results
+        $("#results").append(output);
+      });
+
+      var buttons = getButtons(prevPageToken, nextPageToken);
+
+      // Display Buttons
+      $("#buttons").append(buttons);
+    }
+  );
+}
