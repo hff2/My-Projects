5286657473073c2666f7b5e24031242a98093a04
hff2 <hophoebeho@gmail.com>
2021-03-01 15:12:19 +0800

    refactor the if else statement

--- Tic_Tac_Toe/script.js
+++ Tic_Tac_Toe/script.js
@@ -12,46 +12,11 @@ $(document).ready(function(){
 	var spot7 = $('#spot7');
 	var spot8 = $('#spot8');
 	var spot9 = $('#spot9');
-	
 	$('#board li').on('click', function(){
-		if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
-			spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
-			spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
-			spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
-			spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
-			spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
-			spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
-			spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o')
-		){
-			alert('Winner: O');
-			$('#board li').text('+');
-			$('#board li').removeClass('disable');
-			$('#board li').removeClass('o');
-			$('#board li').removeClass('x');
-		} else if(spot1.hasClass('x') && spot2.hasClass('x') && spot3.hasClass('x') ||
-			spot4.hasClass('x') && spot5.hasClass('x') && spot6.hasClass('x') ||
-			spot7.hasClass('x') && spot8.hasClass('x') && spot9.hasClass('x') ||
-			spot1.hasClass('x') && spot4.hasClass('x') && spot7.hasClass('x') ||
-			spot2.hasClass('x') && spot5.hasClass('x') && spot8.hasClass('x') ||
-			spot3.hasClass('x') && spot6.hasClass('x') && spot9.hasClass('x') ||
-			spot1.hasClass('x') && spot5.hasClass('x') && spot9.hasClass('x') ||
-			spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x')
-		){
-			alert('Winner: X');
-			$('#board li').text('+');
-			$('#board li').removeClass('disable');
-			$('#board li').removeClass('o');
-			$('#board li').removeClass('x');
-		} else if(turns == 9){
-			alert('Tie Game');
-			$('#board li').text('+');
-			$('#board li').removeClass('disable');
-			$('#board li').removeClass('o');
-			$('#board li').removeClass('x');
-			turns = 0;
-		} else if($(this).hasClass('disable')){
+        if($(this).hasClass('disable')){
 			alert('This spot is already filled');
-		} else if(turns%2 == 0){
+		}
+        else if(turns%2 == 0){
 			turns++;
 			$(this).text(o);
 			$(this).addClass('disable o');
@@ -64,10 +29,15 @@ $(document).ready(function(){
 				spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
 				spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o')
 			){
-				alert('Winner: O');
+                alert('Winner: O');
+                $('#board li').text('+');
+                $('#board li').removeClass('disable');
+                $('#board li').removeClass('o');
+                $('#board li').removeClass('x');
 				turns = 0;
 			}
-		} else {
+		} 
+        else if(turns%2 == 1) {
 			turns++;
 			$(this).text(x);
 			$(this).addClass('disable x');
@@ -81,16 +51,28 @@ $(document).ready(function(){
 				spot3.hasClass('x') && spot5.hasClass('x') && spot7.hasClass('x')
 			){
 			alert('Winner: X');
+            $('#board li').text('+');
+			$('#board li').removeClass('disable');
+			$('#board li').removeClass('o');
+			$('#board li').removeClass('x');
 			turns = 0;
 			}
 		}
+        if(turns == 9){
+			alert('Tie Game');
+			$('#board li').text('+');
+			$('#board li').removeClass('disable');
+			$('#board li').removeClass('o');
+			$('#board li').removeClass('x');
+			turns = 0;
+		} 
 	});
-		// Reset Handler
+    	// Reset Handler
 		$("#reset").click(function(){
-		$("#board li").text("+");
-		$("#board li").removeClass('disable');
-		$("#board li").removeClass('o');
-		$("#board li").removeClass('x');
-		turns = 0;
-	});
+            $("#board li").text("+");
+            $("#board li").removeClass('disable');
+            $("#board li").removeClass('o');
+            $("#board li").removeClass('x');
+            turns = 0;
+        });
 });
