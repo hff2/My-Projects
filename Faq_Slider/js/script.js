e8304c4fecca5684b7246528f5126dcbabe205a8
hff2 <hophoebeho@gmail.com>
2021-03-02 14:26:35 +0800

    implement the rotate img when toggle

--- Faq_Slider/js/script.js
+++ Faq_Slider/js/script.js
@@ -10,6 +10,13 @@ $(document).ready(function () {
                 // Select all other answers
                 .siblings("li.a")
                     .slideUp();
+
+        //Get image for active question
+        var img = $(this).children('img');
+        //Remove the 'rotate' class for all images except the active
+        $('img').not(img).removeClass('rotate');
+        // Toggle rotate class
+        img.toggleClass('rotate');
     });
 });
 
